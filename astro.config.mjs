d72a3e2ea13741f0e1e0c1c60960a25d6e4bf623
Andrii Shapovalov <ashxdev@gmail.com>
2024-07-03 17:26:19 +0300

    ad site and base config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://ashxdev.github.io',
+  base: 'the-last-of-mars',
 });
