b5d8d4ec453e97910e1ed3347aaebaa44d4a10c5
Andrii Shapovalov <ashxdev@gmail.com>
2024-09-13 11:03:59 +0300

    up content

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://ashxdev.github.io',
-  base: 'the-last-of-mars',
+  base: 'the-cybermode',
 });
